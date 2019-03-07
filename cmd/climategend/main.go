8a6f4a2fa534d2acf1264d5c7d95c48c0bafd878
Ben Overmyer <ben.overmyer@protolabs.com>
2019-03-07 13:19:31 -0600

    Go mod init

--- cmd/climategend/main.go
+++ cmd/climategend/main.go
@@ -1,33 +1,32 @@
 package main
 
 import (
-  "math/rand"
+	"math/rand"
 
-  "github.com/ironarachne/climategen"
-  "github.com/ironarachne/utility"
-  "github.com/kataras/iris"
+	"github.com/ironarachne/climategen"
+	"github.com/kataras/iris"
 )
 
 func main() {
-  app := iris.New()
+	app := iris.New()
 
-  app.Get("/", func(ctx iris.Context) {
-    ctx.Writef("climategend")
-  })
+	app.Get("/", func(ctx iris.Context) {
+		ctx.Writef("climategend")
+	})
 
-  app.Get("/{id:int64}", func(ctx iris.Context) {
-    id, err := ctx.Params().GetInt64("id")
-    if err != nil {
-      ctx.Writef("error while trying to parse id parameter")
-      ctx.StatusCode(iris.StatusBadRequest)
-      return
-    }
+	app.Get("/{id:int64}", func(ctx iris.Context) {
+		id, err := ctx.Params().GetInt64("id")
+		if err != nil {
+			ctx.Writef("error while trying to parse id parameter")
+			ctx.StatusCode(iris.StatusBadRequest)
+			return
+		}
 
-    rand.Seed(id)
-    climate := climategen.Generate()
+		rand.Seed(id)
+		climate := climategen.Generate()
 
-    ctx.JSON(climate)
-  })
+		ctx.JSON(climate)
+	})
 
-  app.Run(iris.Addr(":7515"))
+	app.Run(iris.Addr(":7515"))
 }
