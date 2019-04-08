5889bf5ec55ba96d725a32501bb7aa07adbf4870
Ben Overmyer <ben.overmyer@protolabs.com>
2019-04-08 10:48:58 -0500

    Standardize mineral data

--- minerals.go
+++ minerals.go
@@ -62,212 +62,209 @@ func getAllMinerals() []Mineral {
 }
 
 func getGems() []Mineral {
-	diamond := Mineral{
-		Name:         "diamond",
-		PluralName:   "diamonds",
-		Hardness:     10,
-		HasOre:       true,
-		IsEdible:     false,
-		IsGem:        true,
-		IsMetal:      false,
-		IsPrecious:   true,
-		IsStone:      false,
-		Malleability: 1,
+	gems := []Mineral{
+		Mineral{
+			Name:         "diamond",
+			PluralName:   "diamonds",
+			Hardness:     10,
+			HasOre:       true,
+			IsEdible:     false,
+			IsGem:        true,
+			IsMetal:      false,
+			IsPrecious:   true,
+			IsStone:      false,
+			Malleability: 1,
+		},
+		Mineral{
+			Name:         "garnet",
+			PluralName:   "garnets",
+			Hardness:     6,
+			HasOre:       true,
+			IsEdible:     false,
+			IsGem:        true,
+			IsMetal:      false,
+			IsPrecious:   true,
+			IsStone:      false,
+			Malleability: 3,
+		},
+		Mineral{
+			Name:         "ruby",
+			PluralName:   "rubies",
+			Hardness:     6,
+			HasOre:       true,
+			IsEdible:     false,
+			IsGem:        true,
+			IsMetal:      false,
+			IsPrecious:   true,
+			IsStone:      false,
+			Malleability: 3,
+		},
+		Mineral{
+			Name:         "sapphire",
+			PluralName:   "sapphires",
+			Hardness:     6,
+			HasOre:       true,
+			IsEdible:     false,
+			IsGem:        true,
+			IsMetal:      false,
+			IsPrecious:   true,
+			IsStone:      false,
+			Malleability: 3,
+		},
 	}
 
-	garnet := Mineral{
-		Name:         "garnet",
-		PluralName:   "garnets",
-		Hardness:     6,
-		HasOre:       true,
-		IsEdible:     false,
-		IsGem:        true,
-		IsMetal:      false,
-		IsPrecious:   true,
-		IsStone:      false,
-		Malleability: 3,
-	}
-
-	ruby := Mineral{
-		Name:         "ruby",
-		PluralName:   "rubies",
-		Hardness:     6,
-		HasOre:       true,
-		IsEdible:     false,
-		IsGem:        true,
-		IsMetal:      false,
-		IsPrecious:   true,
-		IsStone:      false,
-		Malleability: 3,
-	}
-
-	sapphire := Mineral{
-		Name:         "sapphire",
-		PluralName:   "sapphires",
-		Hardness:     6,
-		HasOre:       true,
-		IsEdible:     false,
-		IsGem:        true,
-		IsMetal:      false,
-		IsPrecious:   true,
-		IsStone:      false,
-		Malleability: 3,
-	}
-
-	return []Mineral{diamond, garnet, ruby, sapphire}
+	return gems
 }
 
 func getMetals() []Mineral {
-	copper := Mineral{
-		Name:         "copper",
-		PluralName:   "copper",
-		Hardness:     5,
-		HasOre:       true,
-		IsEdible:     false,
-		IsGem:        false,
-		IsMetal:      true,
-		IsPrecious:   true,
-		IsStone:      false,
-		Malleability: 5,
+	metals := []Mineral{
+		Mineral{
+			Name:         "copper",
+			PluralName:   "copper",
+			Hardness:     5,
+			HasOre:       true,
+			IsEdible:     false,
+			IsGem:        false,
+			IsMetal:      true,
+			IsPrecious:   true,
+			IsStone:      false,
+			Malleability: 5,
+		},
+		Mineral{
+			Name:         "gold",
+			PluralName:   "gold",
+			Hardness:     1,
+			HasOre:       true,
+			IsEdible:     true,
+			IsGem:        false,
+			IsMetal:      true,
+			IsPrecious:   true,
+			IsStone:      false,
+			Malleability: 8,
+		},
+		Mineral{
+			Name:         "iron",
+			PluralName:   "iron",
+			Hardness:     8,
+			HasOre:       true,
+			IsEdible:     false,
+			IsGem:        false,
+			IsMetal:      true,
+			IsPrecious:   false,
+			IsStone:      false,
+			Malleability: 4,
+		},
+		Mineral{
+			Name:         "nickel",
+			PluralName:   "nickel",
+			Hardness:     3,
+			HasOre:       true,
+			IsEdible:     false,
+			IsGem:        false,
+			IsMetal:      true,
+			IsPrecious:   false,
+			IsStone:      false,
+			Malleability: 4,
+		},
+		Mineral{
+			Name:         "silver",
+			PluralName:   "silver",
+			Hardness:     1,
+			HasOre:       true,
+			IsEdible:     false,
+			IsGem:        false,
+			IsMetal:      true,
+			IsPrecious:   true,
+			IsStone:      false,
+			Malleability: 7,
+		},
 	}
 
-	gold := Mineral{
-		Name:         "gold",
-		PluralName:   "gold",
-		Hardness:     1,
-		HasOre:       true,
-		IsEdible:     true,
-		IsGem:        false,
-		IsMetal:      true,
-		IsPrecious:   true,
-		IsStone:      false,
-		Malleability: 8,
-	}
-
-	iron := Mineral{
-		Name:         "iron",
-		PluralName:   "iron",
-		Hardness:     8,
-		HasOre:       true,
-		IsEdible:     false,
-		IsGem:        false,
-		IsMetal:      true,
-		IsPrecious:   false,
-		IsStone:      false,
-		Malleability: 4,
-	}
-
-	nickel := Mineral{
-		Name:         "nickel",
-		PluralName:   "nickel",
-		Hardness:     3,
-		HasOre:       true,
-		IsEdible:     false,
-		IsGem:        false,
-		IsMetal:      true,
-		IsPrecious:   false,
-		IsStone:      false,
-		Malleability: 4,
-	}
-
-	silver := Mineral{
-		Name:         "silver",
-		PluralName:   "silver",
-		Hardness:     1,
-		HasOre:       true,
-		IsEdible:     false,
-		IsGem:        false,
-		IsMetal:      true,
-		IsPrecious:   true,
-		IsStone:      false,
-		Malleability: 7,
-	}
-
-	return []Mineral{copper, gold, iron, nickel, silver}
+	return metals
 }
 
 func getOtherMinerals() []Mineral {
-	coal := Mineral{
-		Name:         "coal",
-		PluralName:   "coal",
-		Hardness:     1,
-		HasOre:       true,
-		IsEdible:     false,
-		IsGem:        false,
-		IsMetal:      false,
-		IsPrecious:   false,
-		IsStone:      false,
-		Malleability: 4,
+	others := []Mineral{
+		Mineral{
+			Name:         "coal",
+			PluralName:   "coal",
+			Hardness:     1,
+			HasOre:       true,
+			IsEdible:     false,
+			IsGem:        false,
+			IsMetal:      false,
+			IsPrecious:   false,
+			IsStone:      false,
+			Malleability: 4,
+		},
+		Mineral{
+			Name:         "salt",
+			PluralName:   "salt",
+			Hardness:     1,
+			HasOre:       false,
+			IsEdible:     true,
+			IsGem:        false,
+			IsMetal:      false,
+			IsPrecious:   false,
+			IsStone:      false,
+			Malleability: 1,
+		},
 	}
 
-	salt := Mineral{
-		Name:         "salt",
-		PluralName:   "salt",
-		Hardness:     1,
-		HasOre:       false,
-		IsEdible:     true,
-		IsGem:        false,
-		IsMetal:      false,
-		IsPrecious:   false,
-		IsStone:      false,
-		Malleability: 1,
-	}
-
-	return []Mineral{coal, salt}
+	return others
 }
 
 func getStones() []Mineral {
-	granite := Mineral{
-		Name:         "granite",
-		PluralName:   "granite",
-		Hardness:     6,
-		HasOre:       false,
-		IsEdible:     false,
-		IsGem:        false,
-		IsMetal:      false,
-		IsPrecious:   false,
-		IsStone:      true,
-		Malleability: 2,
+	stones := []Mineral{
+		Mineral{
+			Name:         "granite",
+			PluralName:   "granite",
+			Hardness:     6,
+			HasOre:       false,
+			IsEdible:     false,
+			IsGem:        false,
+			IsMetal:      false,
+			IsPrecious:   false,
+			IsStone:      true,
+			Malleability: 2,
+		},
+		Mineral{
+			Name:         "limestone",
+			PluralName:   "limestone",
+			Hardness:     4,
+			HasOre:       false,
+			IsEdible:     false,
+			IsGem:        false,
+			IsMetal:      false,
+			IsPrecious:   false,
+			IsStone:      true,
+			Malleability: 4,
+		},
+		Mineral{
+			Name:         "marble",
+			PluralName:   "marble",
+			Hardness:     5,
+			HasOre:       false,
+			IsEdible:     false,
+			IsGem:        false,
+			IsMetal:      false,
+			IsPrecious:   false,
+			IsStone:      true,
+			Malleability: 2,
+		},
+		Mineral{
+			Name:         "sandstone",
+			PluralName:   "sandstone",
+			Hardness:     4,
+			HasOre:       false,
+			IsEdible:     false,
+			IsGem:        false,
+			IsMetal:      false,
+			IsPrecious:   false,
+			IsStone:      true,
+			Malleability: 4,
+		},
 	}
 
-	limestone := Mineral{
-		Name:         "limestone",
-		PluralName:   "limestone",
-		Hardness:     4,
-		HasOre:       false,
-		IsEdible:     false,
-		IsGem:        false,
-		IsMetal:      false,
-		IsPrecious:   false,
-		IsStone:      true,
-		Malleability: 4,
-	}
-
-	marble := Mineral{
-		Name:         "marble",
-		PluralName:   "marble",
-		Hardness:     5,
-		HasOre:       false,
-		IsEdible:     false,
-		IsGem:        false,
-		IsMetal:      false,
-		IsPrecious:   false,
-		IsStone:      true,
-		Malleability: 2,
-	}
-
-	sandstone := Mineral{
-		Name:         "sandstone",
-		PluralName:   "sandstone",
-		Hardness:     4,
-		HasOre:       false,
-		IsEdible:     false,
-		IsGem:        false,
-		IsMetal:      false,
-		IsPrecious:   false,
-		IsStone:      true,
-		Malleability: 4,
-	}
-
-	return []Mineral{granite, limestone, marble, sandstone}
+	return stones
 }
